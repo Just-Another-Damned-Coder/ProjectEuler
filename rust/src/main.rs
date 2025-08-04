a2940777236fd759a80e3663edc4c0591461b6c9
Moris Johnson <luv1tusmaximus@outlook.com>
2025-08-04 06:17:42 +0000

    Re-factored code for speed.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,46 @@
-use std::path::PathBuf;
 extern crate clap;
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use sysinfo::{System, RefreshKind, CpuRefreshKind};
+use std::thread::sleep;
+
+
 mod solutuions;
 
-
-#[derive(Parser, Debug)]
-#[command(about)]
+#[derive(Parser)]
+#[command(name="Project Euler", about)]
 struct Args {
-    // The problem number that we want to solve.
-    #[arg(short, long, default_value_t=1)]
-    number: u8,
-
-    // The file with example test cases.
-    #[arg(short, long, value_name="data/one.txt")]
-    filename: PathBuf,
+    #[command(subcommand)]
+    command: Command,
 }
-
+#[derive(Subcommand)]
+enum Command {
+    #[command(name = "Multiples of 3 or 5")]  // Maps argument to the function
+    One(solutuions::one::MultiplesOf3and5),
+    // Add other tasks with names "2", "3", etc.
+}
 fn main() {
-    
+    let mut sys = System::new_all();
+    let pid = sysinfo::get_current_pid().expect("Unable to get Current PID.");
+    // First update to get accurate initial CPU stats
+    sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    sys.refresh_all();
+
     let arguments: Args = Args::parse();
-    match arguments.number{
-        1 => solutuions::one::solve(arguments.filename.to_str().expect("Did not recieve correct filename, please check again.")),
-        _ => solutuions::one::solve("data/one.txt"),
+    match arguments.command{
+        Command::One(arguments) => solutuions::one::solve(arguments),
     }
+
+    // Refresh all stats again before reading process info
+    sys.refresh_all();
+
+    // Get process info by PID
+    if let Some(process) = sys.process(pid) {
+        println!("Stats for process with PID {}:", pid);
+        println!("CPU usage: {:.2}%", process.cpu_usage());
+        println!("Memory usage: {} MB", process.memory()as f64 / 1024.0);
+        println!("Virtual memory usage: {} MB", process.virtual_memory()as f64 / 1024.0);
+    } else {
+        println!("No process found with PID {}", pid);
+    }
+    println!("Total memory: {} MB", sys.total_memory() as f64 / 1024.0);
 }
--- rust/src/solutuions/one.rs
+++ rust/src/solutuions/one.rs
@@ -1,5 +1,20 @@
 use std::fs;
+use std::path::PathBuf;
 use std::process::exit;
+use std::collections::HashMap;
+
+use clap::Args;
+
+#[derive(Args, Debug)]
+pub struct MultiplesOf3and5 {
+    // The filename containing test cases.
+    #[arg(short, long)]
+    pub filename: Option<PathBuf>,
+
+    // A number to calculate the multiples for.
+    #[arg(short, long)]
+    pub number: Option<u64>,
+}
 
 fn process_exit(filename: &str) {
     println!("The file {filename} does not exist -_- Are you serious ?");
@@ -19,8 +34,8 @@ fn check_file(filename: &str) -> bool{
     return false;
 }
 
-
-pub fn solve(filename: &str){
+fn solvefile(filename: PathBuf){
+    let filename = filename.to_str().expect("Unable to read the filename.");
     match fs::exists(filename) {
         Ok(true) => (),
         _ => process_exit(filename),
@@ -32,14 +47,68 @@ pub fn solve(filename: &str){
     println!("For each test case, you get a number N. Your task is to find the sum of all natural numbers less than N that are multiples of 3 or 5.");
     let contents: String = fs::read_to_string(filename)
         .expect("Should have been able to read the file");
+    let mut numbers: Vec<u64> = vec![];
     for line in contents.lines().skip(1) {
-        let number: u64 = line.parse().unwrap();
-        let mut sum: u64 = 0;
-        for n in 1..number{
+        let number: u64  = line.parse().expect("Not a number.");
+        for n in 1..number {
             if n%3 ==0 || n%5 ==0 {
-                sum = sum + n as u64;
+                numbers.push(n);
             }
         }
+        let sum: u64 = numbers.iter().sum();
         println!("{}", sum);
     }
+}
+
+
+fn solvenum(n: u64) {
+    let mut sum: u64 = 0;
+    for number in 1..n {
+        if number%3 ==0 || number%5 ==0 {
+            sum = sum + number;
+        }
+    }
+    println!("{}", sum);
+}
+
+pub fn solve(arguments: MultiplesOf3and5){
+    match arguments.filename {
+        Some(filename) => solvefile(filename),
+        None => (),
+    }
+    
+    match arguments.number {
+        Some(num) => solvenum(num),
+        None => (),
+    }
+
+
+
+    // let mut numbers: Vec<u64> = vec![];
+    // let mut answers: HashMap<u64, u64> = HashMap::new();
+    // for line in contents.lines().skip(1) {
+    //     let number: u64 = line.parse().unwrap();
+    //     numbers.push(number);
+        
+    // }
+    // let mut numbers_ = numbers.clone();
+    // numbers_.sort();
+    // let (mut number, mut result) = (0 as u64,0 as u64);
+    // for n in numbers_.iter(){
+    //     let multiples: Vec<u64> = (number..*n)
+    //     .filter(|n| n % 3 == 0 || n % 5 == 0)
+    //     .collect();
+    //     let sum: u64 = multiples.iter().sum();
+    //     // println!("{}", sum + result);
+    //     answers.insert(*n, sum + result);
+    //     result = sum;
+    //     number = *n;
+
+    // }
+    // println!("{:?}", answers);
+    // for n in numbers.iter(){
+    //     let value = answers.get(n).unwrap();
+    //     println!("{value}");
+    // }
+    
 }   
